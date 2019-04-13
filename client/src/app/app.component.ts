b3eda650e76bebb16dce61a98ca80a3588202305
Den <merdenchik@gmail.com>
2019-04-13 23:54:30 +0300

    orders

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,13 +12,21 @@ export class AppComponent {
   @Select(DealerState.token)
   public token$: Observable<string>;
 
+  @Select(DealerState.isAddModal)
+  public isAddModal$: Observable<boolean>;
+
   title = 'client';
   isAuthed: boolean = false;
+  isAddModal: boolean;
 
   constructor(private store: Store) {
     this.token$.subscribe((val) => {
       console.log(val);
       this.isAuthed = val.length > 0;
+    });
+
+    this.isAddModal$.subscribe((val) => {
+      this.isAddModal = val;
     });
   }
 }
--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
+import { ToggleAddModal } from './../store/dealer.state';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Store, Select } from '@ngxs/store';
-import { DealerState } from '../store/dealer.state';
+import { DealerState, SetToken } from '../store/dealer.state';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -15,14 +17,67 @@ export class DashboardComponent implements OnInit {
 
   dealerId: string;
 
-  constructor(private store: Store) {
+  stocks: Array<any>;
+  orders: Array<any>;
+
+  constructor(private http: HttpClient, private store: Store) {
     this.dealerId$.subscribe((val) => {
       console.log(val);
       this.dealerId = val;
     });
+
+    this.getStocks();
+    this.getOrders();
+  }
+
+  getStocks() {
+    const token = this.store.selectSnapshot(DealerState.token);
+
+    this.http.get(`http://192.168.10.53:5001/${token}/stock_request`).subscribe((data: any) => {
+      console.log(data);
+      data.sort((a, b) => {
+        if (a.status === 'available' && b.status === 'closed') {
+          return -1;
+        }
+        if (a.status === 'closed' && b.status === 'available') {
+          return 1;
+        }
+
+        return 0;
+      });
+
+      this.stocks = data;
+    });
+  }
+
+  getOrders() {
+    const token = this.store.selectSnapshot(DealerState.token);
+
+    this.http.get(`http://192.168.10.53:5001/${token}/orders`).subscribe((data: any) => {
+      console.log(data);
+
+      this.orders = data;
+    });
   }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.store.dispatch(new SetToken(''));
+  }
+
+  openAddStock() {
+    this.store.dispatch(new ToggleAddModal());
+  }
+
+  close(id) {
+    const token = this.store.selectSnapshot(DealerState.token);
+
+    this.http.get(`http://192.168.10.53:5001/${token}/closing_stock/${id}`).subscribe((data: any) => {
+      console.log(data);
+      this.getStocks();
+    });
   }
 
 }
